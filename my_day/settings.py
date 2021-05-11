62ad5098b7a46e1b94e8a8c0761e617cbfde9bf2
amir <mr.amir.rezazadehh@gmail.com>
2021-05-12 01:26:51 +0430

    Added static files configurations for prod

--- my_day/settings.py
+++ my_day/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 import django_heroku
 from pathlib import Path
 
@@ -121,7 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
